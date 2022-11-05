a8a03c4d15fa070d2ba6b9562fcc731cdb29b34e
naka0303 <jyunki.nakagawa@gmail.com>
2022-11-06 02:25:52 +0900

    機能分割

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,8 @@
-import shutil
-from logging import basicConfig, info, debug, warning, DEBUG, INFO, WARNING
+import os
+from logging import basicConfig, info, debug, warning, error, DEBUG, INFO, WARNING, ERROR
 import datetime
 
-class LOGGER:
+class Logger:
 
     app_path = None
     logs_path = None
@@ -17,22 +17,41 @@ class LOGGER:
         self.logs_path = logs_path
         self.filename = filename
 
-    # 空のログファイル生成用関数
+    # 空のログファイル生成用関数(既に存在すれば生成しない)
     def make_logfile(self):
-        with open(self.logs_path + '/' + LOGGER.logfilename, 'w') as f:
+        logfile_flg = os.path.isfile(self.logs_path + '/' + Logger.logfilename)
+        if logfile_flg:
+            return 0
+
+        with open(self.logs_path + '/' + Logger.logfilename, 'w') as f:
             f.write('')
+
+        return 0
         
     def info(self, log_msg):
-        basicConfig(filename=self.logs_path + '/' + LOGGER.logfilename, level=INFO)
+        basicConfig(filename=self.logs_path + '/' + Logger.logfilename, level=INFO)
 
         info(self.filename + ' ' + log_msg)
 
+        return 0
+
     def debug(self, log_msg):
-        basicConfig(filename=self.logs_path + '/' + LOGGER.logfilename, level=DEBUG)
+        basicConfig(filename=self.logs_path + '/' + Logger.logfilename, level=DEBUG)
 
         debug(self.filename + ' ' + log_msg)
 
+        return 0
+
     def warning(self, log_msg):
-        basicConfig(filename=self.logs_path + '/' + LOGGER.logfilename, level=WARNING)
+        basicConfig(filename=self.logs_path + '/' + Logger.logfilename, level=WARNING)
 
         warning(self.filename + ' ' + log_msg)
+
+        return 0
+
+    def error(self, log_msg):
+        basicConfig(filename=self.logs_path + '/' + Logger.logfilename, level=ERROR)
+
+        error(self.filename + ' ' + log_msg)
+
+        return 0

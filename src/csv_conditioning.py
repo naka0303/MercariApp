7471171932da642935c70638ea954b230c897b8e
naka0303 <jyunki.nakagawa@gmail.com>
2022-11-07 00:33:01 +0900

    bug fix

--- src/csv_conditioning.py
+++ src/csv_conditioning.py
@@ -11,6 +11,7 @@ class CsvConditioning:
 
     # メルカリ画面から取得した商品名と価格の配列から不要情報を除去
     def remove_unneeded(self, args, all_name_price):
+        all_name_price_removed = []
         idx = 0
         for name_price in all_name_price:
             name = name_price[0]
@@ -19,15 +20,13 @@ class CsvConditioning:
             none_cnt = 0
             for arg in args:
                 if (arg in name):
+                    none_cnt += 1
                     break
-
-                # 検索ワードが商品名に無い場合にインクリメント実施
-                none_cnt += 1
             
             # 検索ワード全てが商品名に無ければ配列から削除
-            if (none_cnt == 2):
-                all_name_price.pop(idx)
-            
+            if (none_cnt != 0):
+                all_name_price_removed.append(name_price)
+
             idx += 1
         
-        return all_name_price
+        return all_name_price_removed

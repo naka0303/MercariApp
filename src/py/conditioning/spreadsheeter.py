3996b736934af45c33fb01febf42260cc35297d4
naka0303 <jyunki.nakagawa@gmail.com>
2022-11-18 02:12:34 +0900

    スプレッドシートへの出力方法を変更

--- src/py/conditioning/spreadsheeter.py
+++ src/py/conditioning/spreadsheeter.py
@@ -2,20 +2,27 @@ import os
 import gspread
 
 class SpreadSheeter:
-    def make_sheet(self, oauth_path, all_idx_name_price_img_removed):
+    def make_sheet(self, oauth_path, yyyymmddhhmmss, all_idx_name_price_img_removed):
         # OAuthキーの利用
         gc = gspread.oauth(
             # client_secretパスを格納
-            credentials_filename=os.path.join(oauth_path, "client_secret_708479444314-fkprkidrq71eab4kogdep1hcac4pvr55.apps.googleusercontent.com.json"),
+            credentials_filename=os.path.join(oauth_path, 'client_secret_708479444314-fkprkidrq71eab4kogdep1hcac4pvr55.apps.googleusercontent.com.json'),
             # 認証ファイルの保存先パスを格納
-            authorized_user_filename=os.path.join(oauth_path, "authorized_user.json")
+            authorized_user_filename=os.path.join(oauth_path, 'authorized_user.json')
         )
 
         # 書き込み先のスプレッドシートの取得
-        wb = gc.open_by_key("1VeC31uBKnkrYm7MrzVag1Fo9f0lZzw4Qw4Ty68W0v5Q")
-        # 書き込むシートを取得
-        ws = wb.worksheet("シート1")
+        wb = gc.open_by_key('1VeC31uBKnkrYm7MrzVag1Fo9f0lZzw4Qw4Ty68W0v5Q')
+
+        # 書き込むシートを作成
+        ws = wb.add_worksheet(title=str(yyyymmddhhmmss), rows=100, cols=5)
+        cell_list = ws.range('A1:D100')
+
+        # 書き込み用リスト作成
+        for i, j in enumerate(all_idx_name_price_img_removed):
+            for k, data in enumerate(j):
+                updateNum = i * 4 + k
+                cell_list[updateNum].value = data
 
         # 書き込み
-        for row in all_idx_name_price_img_removed:
-            ws.append_row([row[0], row[1], row[2], row[3]])
+        ws.update_cells(cell_list, value_input_option='USER_ENTERED')
